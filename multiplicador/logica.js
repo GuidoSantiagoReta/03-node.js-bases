07b653704dadb509e26086d3ad940caf734cab4a
Guido Santiago Reta <guidosantiagoreta@gmail.com>
2022-02-13 18:05:46 -0300

    multiplicador avances

---
+++ multiplicador/logica.js
@@ -0,0 +1,17 @@
+
+
+const logica = (base) =>{
+    let resultado= '';
+
+
+    for (let i = 1; i <= 10; i++) {
+        resultado = resultado + `${base} * ${i} = ${base * i}\n`
+        
+    }
+
+    return resultado;
+}
+
+module.exports ={
+    logica
+}
